6d42ee2754626b3fbd7de4fd9abf5583329c5490
Asheem Rahman <mhdasheem@gmail.com>
2026-07-14 03:08:33 +0530

    Fix : Product schema

--- Backend/src/model/productSchema.js
+++ Backend/src/model/productSchema.js
@@ -1,21 +1,5 @@
 import mongoose, { Schema } from "mongoose";
 
-const stockEntrySchema = new mongoose.Schema(
-    {
-        store: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Store',
-            required: true,
-        },
-        quantity: {
-            type: Number,
-            required: true,
-            default: 0,
-            min: 0,
-        },
-    },
-    { _id: false }
-);
 
 const productSchema = new Schema({
     name: {
@@ -26,10 +10,6 @@ const productSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-    },
-    stock: {
-        type: [stockEntrySchema],
-        default: [],
     },
 }, { timestamps: true, }
 );
