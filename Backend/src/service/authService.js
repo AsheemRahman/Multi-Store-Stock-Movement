0a6032f159a5f008026380b2ba38917f5c4af271
Asheem Rahman <mhdasheem@gmail.com>
2026-07-14 17:01:30 +0530

    fea : password hashing in seperate folder

--- Backend/src/service/authService.js
+++ Backend/src/service/authService.js
@@ -1,31 +1,28 @@
 import bcrypt from "bcryptjs";
 import authRepository from "../repository/authRepository.js";
+import PasswordUtils from "../utils/passwordUtils.js";
+
 
 class AuthService {
     async register(email, password, role) {
         const existingUser = await authRepository.findByEmail(email);
-
         if (existingUser) {
             throw new Error("EMAIL_ALREADY_EXISTS");
         }
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await PasswordUtils.passwordHash(password, 10);
         const user = await authRepository.createUser({ email: email.toLowerCase(), password: hashedPassword, role,});
         return user;
     }
 
     async login(email, password) {
         const user = await authRepository.findByEmail(email);
-
         if (!user) {
             throw new Error("INVALID_CREDENTIALS");
         }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
+        const isMatch = await PasswordUtils.comparePassword(password, user.password);
         if (!isMatch) {
             throw new Error("INVALID_CREDENTIALS");
         }
-
         return user;
     }
 }
